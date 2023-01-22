a20b676aa4f14d9fa92d4f457b5eee0ba5f0cadd
K-Jacko <wakumx@icloud.com>
2023-01-22 12:42:31 +0000

    INIT

---
+++ Assets/Scripts/SceneDirector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneDirector : MonoBehaviour
+{
+    // Press button to start
+    // Pattern launch animation 
+    // Wave out prep wave in 
+    // Pick list of mobs based on probablility
+    // Wave in
+    // mob spawns with random color 
+    // half way through spawning the wave pattern starts 
+    // after pattern completes gun activates
+    // kill
+
+    private List<Entity> mobs;
+    private 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
